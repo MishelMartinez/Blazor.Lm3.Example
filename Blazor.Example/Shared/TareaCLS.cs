350c4c642ce1278d744d4fca553eb02086d0fef9
MishelMartinez <lilian.mishel06@gmail.com>
2022-12-21 18:36:00 -0600

    Agregar archivos de proyecto.

---
+++ Blazor.Example/Shared/TareaCLS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.Example.Shared
+{
+    public  class TareaCLS
+    {
+
+        public string dayWeek { get; set; }
+
+        public string Homework { get; set; }
+
+        public Boolean chkCheck { get; set; }
+
+    }
+}
